4202dd71060755a2c4168ab6ce2bc17880197197
Nicholas <mullally.nt@gmail.com>
2022-06-05 15:01:45 -0700

    Done with assignment

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,8 @@ function About() {
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <p>https://github.com/liza</p>
+      <p>https://www.linkedin.com/in/liza</p>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,8 @@
+function Links() {
+    return (
+      <div>
+          <h3>Links</h3>
+          <a href="https://github.com/liza"></a>
+      </div>
+    );
+  }
